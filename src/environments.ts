f7d8185c3ec6ee2d39f6c9b55ff9955943aef2c9
Ashik-Skills <ashik@skills-agency.com>
2023-01-16 22:50:13 +0530

    changes

--- src/environments.ts
+++ src/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     contentful: {
-      baseUrl : 'http://localhost/backend-main/orders.php',
+      baseUrl : 'http://localhost/backend-main/orders',
     }
 };
